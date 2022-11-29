7d2e069a71f1246c6b7baf67ab6f2382711d68f4
QJoly <qjoly@users.noreply.github.com>
2022-11-29 10:48:41 +0100

    Ajout de la configuration zsh

--- dotfiles.nix
+++ dotfiles.nix
@@ -1,4 +1,6 @@
 { pkgs, inputs, system, ... }:
 {
   home.file.".config/user-dirs.dirs".source = ./dotfiles/user-dirs.dirs;
+  home.file.".zshrc".source = ./dotfiles/zshrc;
+  home.file.".p10k.zsh".source = ./dotfiles/p10k.zsh;
 }
