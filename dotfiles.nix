b616efc8bc33991ce15c17814fc27973ecaff3b6
QJoly <qjoly@users.noreply.github.com>
2022-11-29 15:53:02 +0100

    Mise à jour des dotfiles (zsh et i3)

--- dotfiles.nix
+++ dotfiles.nix
@@ -3,4 +3,8 @@
   home.file.".config/user-dirs.dirs".source = ./dotfiles/user-dirs.dirs;
   home.file.".zshrc".source = ./dotfiles/zshrc;
   home.file.".p10k.zsh".source = ./dotfiles/p10k.zsh;
+
+# i3
+  home.file.".config/.wallpaper.png".source = ./dotfiles/wallpaper.png;
+  home.file.".polybar.sh".source = ./dotfiles/polybar.sh;
 }
