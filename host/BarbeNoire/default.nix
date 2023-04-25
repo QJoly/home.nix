ffe02f587a96346d40812aa11eccef9261122952
QJoly <github@thoughtless.eu>
2023-04-25 12:56:59 +0200

    BarbeNoire has all gui packages & cli

--- host/BarbeNoire/default.nix
+++ host/BarbeNoire/default.nix
@@ -3,8 +3,13 @@
     ../../packages/cli/kubernetes.nix
     ../../packages/cli/terraform.nix
     ../../packages/cli/ansible.nix
+    ../../packages/gui/code.nix
     ../../packages/gui/media.nix
+    ../../packages/gui/misc.nix
+    ../../packages/gui/office.nix
+    ../../packages/gui/social.nix
     ../../config/git.nix
     ../../config/zsh.nix
     ];
+
 }
