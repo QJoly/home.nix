9f04253f3d325dc48bc4ed2400a2102004f5142d
QJoly <github@thoughtless.eu>
2023-04-27 14:00:30 +0200

    Espanso config for barbenoire

--- host/BarbeNoire/default.nix
+++ host/BarbeNoire/default.nix
@@ -1,8 +1,12 @@
-{
+let
+  DOCKER_HOST = "unix:///run/user/1000/docker.sock";
+in
+rec {
   imports = [
     ../../packages/cli/kubernetes.nix
     ../../packages/cli/terraform.nix
     ../../packages/cli/ansible.nix
+    ../../packages/cli/editors.nix
     ../../packages/cli/network.nix
     ../../packages/gui/code.nix
     ../../packages/gui/media.nix
@@ -10,14 +14,17 @@
     ../../packages/gui/office.nix
     ../../packages/gui/social.nix
     ../../config/git.nix
+    ../../config/kubernetes.nix
     ../../config/zsh.nix
-    ./espanso.nix
     ../../config/direnv.nix
+    ../../config/vim.nix
+    ./espanso.nix
+
   ];
 
   programs.zsh = {
     initExtra = ''
-      export DOCKER_HOST=unix:///run/user/1000/docker.sock
+      export DOCKER_HOST=${DOCKER_HOST}
       export SOPS_AGE_KEY_FILE=~/.keys/github
     '';
   };
--- host/BarbeNoire/espanso.nix
+++ host/BarbeNoire/espanso.nix
@@ -1,4 +1,8 @@
-{
+let
+  date_format = "%m/%d/%Y";
+  public_ip = "ifconfig.me";
+in
+rec {
   home.file.".config/espanso/default.yml".text = ''
     matches:
       - trigger: ":espanso"
@@ -10,7 +14,7 @@
           - name: mydate
             type: date
             params:
-              format: "%m/%d/%Y"
+              format: "${date_format}"
 
       - trigger: ":shell"
         replace: "{{output}}"
@@ -19,5 +23,131 @@
             type: shell
             params:
               cmd: "echo Hello from your shell"
+
+      - trigger: ":getip"
+        replace: "{{output}}"
+        vars:
+          - name: output
+            type: shell
+            params:
+              cmd: "curl ${public_ip}"
+
+    # French Accents
+      - trigger: "e'"
+        replace: "é"
+
+      - trigger: "e`"
+        replace: "è"
+
+      - trigger: "e^"
+        replace: "ê"
+
+      - trigger: "e:"
+        replace: "ë"
+
+      - trigger: "o^"
+        replace: "ô"
+
+      - trigger: "a`"
+        replace: "à"
+
+      - trigger: "a^"
+        replace: "â"
+
+      - trigger: "i^"
+        replace: "î"
+
+      - trigger: "i:"
+        replace: "ï"
+
+      - trigger: "u`"
+        replace: "ù"
+
+      - trigger: "u^"
+        replace: "û"
+
+      - trigger: "u:"
+        replace: "ü"
+
+      - trigger: "oe"
+        replace: "œ"
+
+      - trigger: "ae"
+        replace: "æ"
+
+      - trigger: "y:"
+        replace: "ÿ"
+
+      - trigger: "c,"
+        replace: "ç"
+
+      - trigger: "E'"
+        replace: "É"
+
+      - trigger: "E`"
+        replace: "È"
+
+      - trigger: "E^"
+        replace: "Ê"
+
+      - trigger: "E:"
+        replace: "Ë"
+
+      - trigger: "O^"
+        replace: "Ô"
+
+      - trigger: "A`"
+        replace: "À"
+
+      - trigger: "A^"
+        replace: "Â"
+
+      - trigger: "I^"
+        replace: "Î"
+
+      - trigger: "I:"
+        replace: "Ï"
+
+      - trigger: "U`"
+        replace: "Ù"
+
+      - trigger: "U^"
+        replace: "Û"
+
+      - trigger: "U:"
+        replace: "Ü"
+
+      - trigger: "OE"
+        replace: "Œ"
+
+      - trigger: "AE"
+        replace: "Æ"
+
+      - trigger: "Y:"
+        replace: "Ÿ"
+
+      - trigger: "C,"
+        replace: "Ç"
+
+      - trigger: ":dbuild"
+        replace: "docker-compose build"
+        
+      - trigger: ":dup"
+        replace: "docker-compose up"
+
+      - trigger: ":dupd"
+        replace: "docker-compose up -d"
+        
+      - trigger: ":ddown"
+        replace: "docker-compose down"
+
+      - trigger: ":dlogs"
+        replace: "docker-compose logs -f"
+
+      - trigger: ":dexec"
+        replace: "docker-compose exec"
+        
+      - trigger: ":drestart"
+        replace: "docker-compose restart"
   '';
 }
