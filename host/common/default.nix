986bb0029d96c297c41b0908a65b154f58a06675
Quentin JOLY <82603435+QJoly@users.noreply.github.com>
2023-04-28 14:56:34 +0200

    Update default.nix

--- host/common/default.nix
+++ host/common/default.nix
@@ -2,8 +2,5 @@
   imports = [
     ../../config/git.nix
     ../../config/zsh.nix
-    ../../packages/cli/kubernetes.nix
-    ../../packages/cli/terraform.nix
-    ../../packages/cli/ansible.nix
   ];
 }
