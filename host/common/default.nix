64fb9beaa4c59bd5d3df1f05f3bff86bca138aee
QJoly <github@thoughtless.eu>
2023-06-15 06:57:27 +0200

    remove unused packages of "common" host

--- host/common/default.nix
+++ host/common/default.nix
@@ -1,8 +1,5 @@
 {
   imports = [
-    ../../packages/cli/kubernetes.nix
-    ../../packages/cli/terraform.nix
-    ../../packages/cli/ansible.nix
     ../../packages/cli/editors.nix
     ../../packages/cli/network.nix
     ../../config/git.nix
