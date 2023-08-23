7ac98cb48958d2416c3f5b155c521ba71c5811f0
QJoly <github@thoughtless.eu>
2023-08-23 15:16:33 +0200

    update common host

--- host/common/default.nix
+++ host/common/default.nix
@@ -1,11 +1,16 @@
 {
   imports = [
+    ../../packages/cli/ansible.nix
+    ../../packages/cli/dev.nix
     ../../packages/cli/editors.nix
     ../../packages/cli/network.nix
+    ../../packages/cli/kubernetes.nix
+    ../../packages/cli/hashicorp.nix
     ../../config/git.nix
     ../../config/kubernetes.nix
     ../../config/zsh.nix
-    ../../config/direnv.nix
     ../../config/vim.nix
+    ../../config/tmux.nix
+    ../../config/direnv.nix
   ];
 }
